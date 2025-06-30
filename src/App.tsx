be2feabd1fa5130bb1f83b4e4c58403c450fbd7c
Petrushin <petrushin@antivor.ru>
2025-06-30 11:42:41 +0300

    init i18n in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useTranslation } from 'react-i18next';
 import './App.css'
 
+type Languages = 'ru' | 'en' | 'tr';
+type LanguagesInfo = {
+  nativeName: string;
+}
+
+const languages: Record<Languages, LanguagesInfo> = {
+  ru: { nativeName: 'Русский ' },
+  en: { nativeName: 'Английский' },
+  tr: { nativeName: 'Турецкий' },
+};
+
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
     <div>
-      start project
+      <div>
+        {Object.keys(languages).map((language) => (
+          <button key={language} style={{ fontWeight: i18n.resolvedLanguage === language ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(language)}>
+            {languages[language as Languages].nativeName}
+          </button>
+        ))}
+      </div>
+      {t('Отчёты.Количество посетителей')}
     </div>
   )
 }
